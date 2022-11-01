79c82bc0b78b3bfc151945ef4380ff9ddb263159
Madhav070 <114184904+Madhav070@users.noreply.github.com>
2022-11-02 00:35:14 +0530

    hi

---
+++ html/css/x.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    if(n%2==0){
+        for(int i=1;i<=n;i++){
+            for(int i=1;i<=n/2;i++){
+                printf("")
+            }
+
+        }
+    }
+    return 0;
+}
